f913554fdc16f2776cebb2b1c88792d57b5c9fa7
nifemi03 <adefulirenifemi@gmail.com>
2023-09-18 16:12:14 +0100

    dont swap horses

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
 *swap_int  - a fuction that swaps the values of two integers
-*@n: An output character
+*@a: An output character
+*@b:an input character
 *Return: Always 0
 */
 void swap_int(int *a, int *b)
