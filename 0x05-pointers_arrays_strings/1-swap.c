01d66f286ad337732931c744c4987e634e499443
nifemi03 <adefulirenifemi@gmail.com>
2023-09-18 16:06:24 +0100

    dont swap horses

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*swap_int  - a fuction that swaps the values of two integers
+*@n: An output character
+*Return: Always 0
+*/
+void swap_int(int *a, int *b)
+{
+int temp = *a;
+*a = *b;
+*b = temp;
+
+}
