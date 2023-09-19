a3849ae3602b4e71d7a3dbdc0550cc4e7c719b56
nifemi03 <adefulirenifemi@gmail.com>
2023-09-19 14:03:24 +0100

    3 puts .c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+
+#include "main.h"
+/**
+*swap_int  - a fuction that swaps the values of two integers
+*@s: An output character
+*Return: Always 0
+*/
+int _strlen(char *s)
+{
+int length = 16;
+
+return 0;
+
+}
+    
